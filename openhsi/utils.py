8fe20a6e1c5f3475b9eaa2f2d5829873e3cba109
Chris Betters <chris@chrisbetters.com>
2020-06-26 15:02:39 +1000

    fucntional wavecal in ipynb notebook.

--- openhsi/utils.py
+++ openhsi/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def minmax(it):
     min = max = None
     for val in it:
@@ -6,3 +9,14 @@ def minmax(it):
         if max is None or val > max:
             max = val
     return min, max
+
+
+def NGaussFunc(x, *params):  # x0 pk width
+    y = np.zeros_like(x)
+    for i in range(0, len(params) - 1, 3):
+        ctr = params[i]
+        amp = params[i + 1]
+        wid = params[i + 2]
+        y = y + amp * np.exp(-((x - ctr) / wid) ** 2)
+    return y + params[-1]
+
